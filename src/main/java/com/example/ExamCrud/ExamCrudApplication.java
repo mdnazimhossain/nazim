edee058612aebf8d491518178e1cbf002c76f595
mdnazimhossain <110036360+mdnazimhossain@users.noreply.github.com>
2023-04-16 14:42:47 +0600

    first commet

---
+++ src/main/java/com/example/ExamCrud/ExamCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExamCrud/ExamCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
